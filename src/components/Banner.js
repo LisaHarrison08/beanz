537bfbfb343f5c2dc81424fee1841214edec9f96
kurtreyn <kurtreyn@yahoo.com>
2022-05-10 19:01:39 -0400

    created ImageCard & TextCard components added filler content to Home

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import coffeeBarImg from '../assets/coffee-bar.jpg';
+import coffeeBarBanner from '../assets/coffee-bar-banner.jpg';
 
 // Need to resize the banner image's height so it doesn't take up as much room on the page.
 
@@ -7,7 +7,7 @@ function Banner() {
   return (
     <div className="jumbotron custom-jumbotron">
       <img
-        src={coffeeBarImg}
+        src={coffeeBarBanner}
         className="banner-img custom-banner"
         alt="banner"
       />
