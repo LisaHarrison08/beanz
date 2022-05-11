c13c67fc41f4504e7f085ff8da4d36dc4756a259
Lisa Harrison <lmharrison0810@gmail.com>
2022-05-10 21:32:43 -0400

    updated image of about component added text card

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import coffee from '../assets/coffee-cup.jpg';
-import barista from '../assets/barista.jpg';
+import barista from '../assets/people2.jpg';
 import Fade from 'react-reveal';
-// import { Container, Col, Row } from 'react-bootstrap'
+import TextCard from './TextCard';
+
 
 function About() {
   return (
     <div className="row about-row">
       <Fade left duration={1000} distance="500px">
-        <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 about-col">
+        <div className="col-sm-12 col-md-6 about-col">
           <img className="about-us" src={barista} alt="Barista" />
         </div>
       </Fade>
       <Fade right duration={1000} distance="500px">
-        <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 about-col">
-          <img className="about-pic" src={coffee} alt="Coffee Cup" />
+        <div className="col-sm-12 col-md-6 about-col">
+          < TextCard />
         </div>
       </Fade>
     </div>
