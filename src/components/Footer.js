f857a3a3f157b21cbd118161b9f6043f709a5080
Lisa Harrison <lmharrison0810@gmail.com>
2022-05-11 15:40:56 -0400

    site footer text added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
     return (
@@ -9,12 +9,25 @@ function Footer() {
             <div className="container">
                 <div className="row">
                     <div className="col text-center">
-                        <a href="mailto:beanz@gmail.com" id="contact" className="icon-link">
-                            <FontAwesomeIcon icon={faEnvelope} size="3x" />
+                        <h5>BEANZ</h5>
+                        <p>1445 Beanz Ave, New York City, United States <br></br>
+                            <FontAwesomeIcon icon={faPhone} size="1x" /> 123-123-4567</p>
+                    </div>
+                    <div className="col text-center">
+                        <a href="mailto:beanz@gmail.com" className="icon-link">
+                            <FontAwesomeIcon icon={faEnvelope} size="2x" />
                         </a>
                         <a href="https://www.linkedin.com/" className="icon-link">
-                            <FontAwesomeIcon icon={faFacebookSquare} size="3x" />
+                            <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
                         </a>
+                        <a href="https://www.linkedin.com/" className="icon-link">
+                            <FontAwesomeIcon icon={faInstagramSquare} size="2x" />
+                        </a>
+                    </div>
+                    <div className="col text-center">
+                        <h5>OPEN HOURS</h5>
+                        <p>Monday - Friday || 8:00am - 9:00pm<br></br>
+                        Saturday & Sunday || 9:00am - 5:00pm</p>
                     </div>
                 </div>
             </div>
