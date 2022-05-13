5f37d6fdcf0333e2d02dd70803838dc328f7d6fc
Lisa Harrison <lmharrison0810@gmail.com>
2022-05-12 21:42:19 -0400

    nav logo added to nav bar

--- src/components/Navigation2.js
+++ src/components/Navigation2.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { Nav, Navbar } from "react-bootstrap"
+import logo from '../assets/logo2.png';
+
 
 function Navigation() {
     return (
         <nav>
             <Navbar className="nav2" expand="md" fixed="top" activeKey="/home">
+                <Navbar.Brand href="/">
+                    <img src={logo} className="nav-logo" alt="logo" />
+                </Navbar.Brand>
                 <Nav.Link href="/home" className="nav-link">Home</Nav.Link>
-                <Nav.Link href="/about"className="nav-link">About</Nav.Link>
+                <Nav.Link href="/about" className="nav-link">About</Nav.Link>
                 <Nav.Link href="/menu" className="nav-link">Menu</Nav.Link>
             </Navbar>
         </nav>
